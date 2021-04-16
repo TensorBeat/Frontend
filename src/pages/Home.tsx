09bfd66137cb5383b6cda460d9cffb069afea298
LuLaValva <lulavalva@gmail.com>
2021-04-16 15:11:01 -0400

    Removed many homes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,14 @@
 import React from "react";
 import Page from "../components/Page";
 
-export type HomeProps = {
-
-}
+export type HomeProps = {};
 
 export default function Home() {
-
-    return (
-        <div>
-            <Page>
-                <h1>Home1</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1><h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1><h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1><h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Homef</h1>
-
-            </Page>
-        </div>
-    )
-
+  return (
+    <div>
+      <Page>
+        <h1>Welcome to TensorBeat</h1>
+      </Page>
+    </div>
+  );
 }
